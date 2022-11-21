2eb956c9da7b42abcdc84136fb6351a42f7251ef
Zhijiangjwf <orca1stt@gmail.com>
2022-11-21 22:58:41 +0800

    Create DataTypeDemo.java

---
+++ code/DataTypeDemo.java
@@ -0,0 +1,26 @@
+/*
+ * JAVA是一种强类型语言
+ * 强类型是指变量在定义时必须显示声明类型是什么
+ * 弱类型表示，变量会根据值自己去推断，不需要指定类型是什么
+ * JAVA数据类型  
+    基本数据类型（4类8种）  
+        - 整数类型：byte short int long（不同类型表示不同长度）
+            byte:使用一个字节存储，因此范围是-128~127  
+            short:使用两个字节存储，因此范围是-32768~32767  
+            int：使用4个字节存储，因此范围是正负21亿  
+            long：使用8个字节存储，因此范围是.....一般也超不了这个范围  
+        - 浮点类型:float double 
+            float:单精度
+            double：双精度
+        - 字符类型:char  
+        - 布尔类型:boolean  
+            只有true和false两值，在存储的时候占一位 
+ */
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        // byte b = 127;
+        // short s = 111144;
+        // long L = 9999999999L;
+
+    }
+}
