1fa02be68eeeed14b0715cc0894d60cfefb323bc
Zhijiangjwf <orca1stt@gmail.com>
2022-11-21 10:49:59 +0800

    Create 调试.java

---
+++ code/调试.java
@@ -0,0 +1,10 @@
+/*
+ 1.java文件名称必须与public class的名称一致
+ 2.一个java文件只能含有一个public class，但可以拥有多个class
+ 3.
+ */
+public class 调试 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
